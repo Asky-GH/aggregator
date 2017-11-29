2423ac48f3533de68bc7fd8a25388bf52deeb14b
Askhat Abishev <asky.gm@gmail.com>
2017-11-30 01:17:15 +0600

    Add link from main view to link view

--- laravel/resources/views/main.blade.php
+++ laravel/resources/views/main.blade.php
@@ -6,7 +6,11 @@
         <div class="col-md-8 col-md-offset-2">
             @foreach ($links as $link)
             <div class="panel panel-default">
-                <div class="panel-heading">{{ $link->title }}</div>
+                <div class="panel-heading">
+                    <h1>
+                        <a href="{{ action('LinkController@show', $link->id) }}">{{ $link->title }}</a>
+                    </h1>
+                </div>
                 <div class="panel-body">{{ $link->description }}</div>
             </div>
             @endforeach
