a785f7ca763d9e7aed11db2db7eb4b7336797e64
Askhat Abishev <asky.gm@gmail.com>
2017-11-29 17:18:02 +0600

    Add index function to LinkController

--- laravel/app/Http/Controllers/LinkController.php
+++ laravel/app/Http/Controllers/LinkController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Link;
 
 class LinkController extends Controller
 {
-    //
+    public function index(){
+        $links = Link::approved()->get();
+
+        return view('main', compact('links'));
+    }
 }
