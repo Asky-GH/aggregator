2f40c7d4a24d52330abc7ad9bf4df86d066ac989
Askhat Abishev <asky.gm@gmail.com>
2017-12-01 19:00:09 +0600

    Update DatabaseSeeder

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,24 +12,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
 
         DB::table('users')->insert([
             'name' => 'askhat',
             'email' => 'asky.gm@gmail.com',
             'password' => bcrypt('admin'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'remember_token' => str_random(10),
         ]);
         DB::table('users')->insert([
             'name' => 'murat',
             'email' => 'hmurich@mail.ru',
             'password' => bcrypt('user'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'remember_token' => str_random(10),
         ]);
 
         DB::table('statuses')->insert([
             'approved' => false,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('statuses')->insert([
             'approved' => true,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
 
         DB::table('links')->insert([
@@ -37,6 +48,8 @@ class DatabaseSeeder extends Seeder
             'description' => 'One of the best MOOC providers with the latest in Deep Learning, Machine Learning, Web Development and more',
             'user_id' => 1,
             'status_id' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('links')->insert([
             'title' => 'GeekBrains',
@@ -44,79 +57,99 @@ class DatabaseSeeder extends Seeder
             'description' => 'Образовательный портал, который помогает начать карьеру в IT с нуля или получить новые навыки для её развития',
             'user_id' => 2,
             'status_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
 
         DB::table('comments')->insert([
             'user_id' => 2,
             'link_id' => 1,
             'body' => 'Great link! Thank you!',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
 
         DB::table('tags')->insert([
             'name' => 'RU',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('tags')->insert([
             'name' => 'ENG',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('tags')->insert([
             'name' => 'GENERAL',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('tags')->insert([
             'name' => 'FREE',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('tags')->insert([
             'name' => 'PAID',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('tags')->insert([
             'name' => 'PHP',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
         DB::table('tags')->insert([
             'name' => 'HTML',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ]);
 
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 1,
             'tag_id' => 2,
         ]);
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 1,
             'tag_id' => 3,
         ]);
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 1,
             'tag_id' => 4,
         ]);
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 1,
             'tag_id' => 5,
         ]);
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 1,
             'tag_id' => 7,
         ]);
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 2,
             'tag_id' => 1,
         ]);
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 2,
             'tag_id' => 3,
         ]);
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 2,
             'tag_id' => 4,
         ]);
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 2,
             'tag_id' => 5,
         ]);
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 2,
             'tag_id' => 6,
         ]);
-        DB::table('link_tags')->insert([
+        DB::table('link_tag')->insert([
             'link_id' => 2,
             'tag_id' => 7,
         ]);
     }
 }
+
+
