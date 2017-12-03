c4d86575a39a30426bca5bdfd5e6d916fe56edac
Askhat Abishev <asky.gm@gmail.com>
2017-12-03 22:51:58 +0600

    Update 'link' view

--- laravel/resources/views/link.blade.php
+++ laravel/resources/views/link.blade.php
@@ -1,20 +1,13 @@
-@extends('layouts.app')
+@extends('layouts.master')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h1>
-                        {{ $link->title }}
-                    </h1>
-                </div>
-                <div class="panel-body">{{ $link->description }}</div>
-                <p>{{ $link->url }}</p>
-                <p>Comments section</p>
-            </div>
-        </div>
-    </div>
+<div class="blog-post">
+    <h2 class="blog-post-title">{{ $link->title }}</h2>
+
+    <p class="blog-post-meta">{{ $link->created_at }}</p>
+
+    <p>{{ $link->description }}</p>
+
+    <p>{{ $link->url }}</p>
 </div>
 @endsection
