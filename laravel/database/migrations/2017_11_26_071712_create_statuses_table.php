466b48b29354d61eba607cdbb7d2dce03042cef5
Askhat Abishev <asky.gm@gmail.com>
2017-12-04 22:02:33 +0600

    Update migrations (statuses, tags)

--- laravel/database/migrations/2017_11_26_071712_create_statuses_table.php
+++ laravel/database/migrations/2017_11_26_071712_create_statuses_table.php
@@ -16,7 +16,6 @@ class CreateStatusesTable extends Migration
         Schema::create('statuses', function (Blueprint $table) {
             $table->increments('id');
             $table->boolean('approved');
-            $table->timestamps();
         });
     }
 
