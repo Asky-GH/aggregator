e569e54ed3f8dc51c323305bda360e2d371f5935
Askhat Abishev <asky.gm@gmail.com>
2017-12-04 22:04:33 +0600

    Update controllers (Link, Comment)

--- laravel/app/Http/Controllers/CommentController.php
+++ laravel/app/Http/Controllers/CommentController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Link;
+use App\Comment;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Link $link){
+        $this->validate(request(), [
+            'body' => 'required'
+        ]);
+
+        return back();
+    }
 }
--- laravel/app/Http/Controllers/LinkController.php
+++ laravel/app/Http/Controllers/LinkController.php
@@ -8,12 +8,28 @@ use App\Link;
 class LinkController extends Controller
 {
     public function index(){
-        $links = Link::approved()->get();
+        $links = Link::approved()->latest()->get();
 
-        return view('links', compact('links'));
+        return view('links.index', compact('links'));
     }
 
     public function show(Link $link){
-        return view('link', compact('link'));
+        return view('links.show', compact('link'));
+    }
+
+    public function create(){
+        return view('links.create');
+    }
+
+    public function store(){
+        $this->validate(request(), [
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'required',
+        ]);
+
+        Link::create(request(['title', 'description', 'url']));
+
+        return redirect('/links');
     }
 }
