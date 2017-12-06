ae9f052dc7e0c2c585f497b354fc73850eea4813
Askhat Abishev <asky.gm@gmail.com>
2017-12-06 16:23:53 +0600

    Change return view from 'home' to 'links.index'

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Link;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $links = Link::approved()->latest()->get();
+        
+        return view('links.index', compact('links'));
     }
 }
