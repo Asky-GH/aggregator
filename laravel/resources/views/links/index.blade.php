4ed20b62f5da35e323fb03a15f752c93ab04f84b
Askhat Abishev <asky.gm@gmail.com>
2017-12-08 02:41:54 +0600

    Add tag rendering for each related link

--- laravel/resources/views/links/index.blade.php
+++ laravel/resources/views/links/index.blade.php
@@ -12,6 +12,12 @@
 
     <p class="blog-post-meta">{{ $link->created_at->toFormattedDateString() }}</p>
 
+    <h5>
+        @foreach ($link->tags as $tag)
+        <span class="label label-danger">{{ $tag->name }}</span>
+        @endforeach
+    </h5><br>
+
     <p>{{ $link->description }}</p>
 </div>
 @endforeach
